08ebd8069faaa5ffc38fd55cac4ca4903911e91a
zaid <gabozaidinde@gmail.com>
2025-09-08 19:41:35 -0600

    Add authentication, email verification, and user settings functionality
    - Implemented authentication routes and views including login, registration, password reset, and email verification.
    - Created console command for displaying inspiring quotes.
    - Added dashboard and settings routes with corresponding views.
    - Established .gitignore files for various storage directories.
    - Developed comprehensive feature tests for authentication, email verification, password confirmation, password reset, registration, and user settings.
    - Configured Vite for asset management with Laravel and Tailwind CSS.
    - Initialized package-lock.json for dependency management.

---
+++ Laravel_framework/basic/app/Http/Controllers/ComidaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class ComidaController extends Controller
+{
+    public function index(){
+
+    }
+
+
+
+    private function mostrarComida(){
+
+    }
+}
+
